b6966ec9cc174a40ca817ad372056b2d18a61bd1
Sabbir <87799340+sabbir4212@users.noreply.github.com>
2022-11-29 10:12:16 +0600

    Update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -105,3 +105,24 @@ exports.profilePictureUploadController = (req, res) => {
         })
     }
 }
+
+
+// send file
+var http = require('http'),
+    fileSystem = require('fs'),
+    path = require('path');
+
+http.createServer(function(request, response) {
+    var filePath = path.join(__dirname, 'myfile.mp3');
+    var stat = fileSystem.statSync(filePath);
+
+    response.writeHead(200, {
+        'Content-Type': 'audio/mpeg',
+        'Content-Length': stat.size
+    });
+
+    var readStream = fileSystem.createReadStream(filePath);
+    // We replaced all the event handlers with a simple call to readStream.pipe()
+    readStream.pipe(response);
+})
+.listen(5000)
